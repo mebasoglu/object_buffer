03abe4ff1f7a9083184dba18550f97c34d47e503
Mehmet Emin BAŞOĞLU <mehmeteminbasoglu@hotmail.com>
2025-02-25 15:09:52 +0300

    feat(object_buffer): overwrite label and size
    Signed-off-by: Mehmet Emin BAŞOĞLU <mehmeteminbasoglu@hotmail.com>

--- object_buffer/src/object_buffer.cpp
+++ object_buffer/src/object_buffer.cpp
@@ -1,11 +1,40 @@
 #include "object_buffer.hpp"
 
+#include "autoware/object_recognition_utils/object_recognition_utils.hpp"
+
 namespace object_buffer
 {
 ObjectBuffer::ObjectBuffer(const rclcpp::NodeOptions & node_options)
 : Node("object_buffer", node_options)
 {
+  RCLCPP_INFO_STREAM(get_logger(), "Hello");
+
+  sub_objects_ = create_subscription<DetectedObjects>(
+    "~/input/objects", rclcpp::QoS{1},
+    std::bind(&ObjectBuffer::onObjects, this, std::placeholders::_1));
+
+  pub_objects_ = create_publisher<DetectedObjects>("~/output/objects", 1);
 }
+
+void ObjectBuffer::onObjects(const DetectedObjects & msg) const
+{
+  DetectedObjects output_objects;
+  output_objects.header = msg.header;
+
+  static const auto label = autoware::object_recognition_utils::toLabel("CAR");
+
+  for (auto object : msg.objects) {
+    object.classification.at(0).label = label;
+    object.shape.type = autoware_perception_msgs::msg::Shape::BOUNDING_BOX;
+    object.shape.dimensions.x = 1.0;
+    object.shape.dimensions.y = 1.0;
+    object.shape.dimensions.z = 1.0;
+    output_objects.objects.push_back(object);
+  }
+
+  pub_objects_->publish(output_objects);
+}
+
 }  // namespace object_buffer
 
 #include "rclcpp_components/register_node_macro.hpp"
--- object_buffer/src/object_buffer.hpp
+++ object_buffer/src/object_buffer.hpp
@@ -1,8 +1,14 @@
 #pragma once
 #include "rclcpp/rclcpp.hpp"
 
+#include "autoware_perception_msgs/msg/detected_objects.hpp"
+
+#include <memory>
+
 namespace object_buffer
 {
+using autoware_perception_msgs::msg::DetectedObject;
+using autoware_perception_msgs::msg::DetectedObjects;
 
 class ObjectBuffer : public rclcpp::Node
 {
@@ -10,6 +16,10 @@ public:
   explicit ObjectBuffer(const rclcpp::NodeOptions & node_options);
 
 private:
+  void onObjects(const DetectedObjects & msg) const;
+
+  rclcpp::Subscription<DetectedObjects>::SharedPtr sub_objects_;
+  rclcpp::Publisher<DetectedObjects>::SharedPtr pub_objects_;
 };
 
 }  // namespace object_buffer
